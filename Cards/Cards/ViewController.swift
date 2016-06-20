2a210277ebffd2813659b83c3f6f6bc1c795e3fb
Michael Dippery <michael@monkey-robot.com>
2016-06-19 17:28:31 -0700

    Complete the app

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var middleLabel: UILabel!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func update(middle middle: String, corners: String) {
+        middleLabel.text = middle
+        topLabel.text = corners
+        bottomLabel.text = corners
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func fourOfClubsPressed(sender: AnyObject) {
+        update(middle: "4", corners: "♣️")
     }
 
+    @IBAction func threeOfSpadesPressed(sender: AnyObject) {
+        update(middle: "3", corners: "♠️")
+    }
 
+    @IBAction func eightOfDiamondsPressed(sender: AnyObject) {
+        update(middle: "8", corners: "♦️")
+    }
+
+    @IBAction func tenOfHeartsPressed(sender: AnyObject) {
+        update(middle: "10", corners: "♥️")
+    }
 }
-
