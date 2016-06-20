2beb1d5ed1fb570c3786762859b031a759826de0
Michael Dippery <michael@monkey-robot.com>
2016-06-19 17:36:44 -0700

    Remove the guts of the application
    This is all code the student has to implement.

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,29 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var middleLabel: UILabel!
-    @IBOutlet weak var topLabel: UILabel!
-    @IBOutlet weak var bottomLabel: UILabel!
+    // TODO: Create IB outlets
 
-    func update(middle middle: String, corners: String) {
-        middleLabel.text = middle
-        topLabel.text = corners
-        bottomLabel.text = corners
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
-    @IBAction func fourOfClubsPressed(sender: AnyObject) {
-        update(middle: "4", corners: "♣️")
-    }
-
-    @IBAction func threeOfSpadesPressed(sender: AnyObject) {
-        update(middle: "3", corners: "♠️")
-    }
-
-    @IBAction func eightOfDiamondsPressed(sender: AnyObject) {
-        update(middle: "8", corners: "♦️")
-    }
-
-    @IBAction func tenOfHeartsPressed(sender: AnyObject) {
-        update(middle: "10", corners: "♥️")
-    }
+    // TODO: IB actions and code to update UI
 }
